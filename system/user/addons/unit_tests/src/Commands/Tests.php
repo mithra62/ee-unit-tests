61d2a045af4a2a3f622e52c8fee2e069681be9f7
Eric Lamb <eric@mithra62.com>
2021-11-24 15:48:25 -0700

    adds phpunit.xml support

--- system/user/addons/unit_tests/src/Commands/Tests.php
+++ system/user/addons/unit_tests/src/Commands/Tests.php
@@ -80,10 +80,15 @@ class Tests extends Cli
             );
         }
 
-        $_SERVER['argv'] = [
-            'phpunit',
-            $tests_path
-        ];
+        $args = ['phpunit'];
+        $xml_file = $tests_path.'/phpunit.xml';
+        if(file_exists($xml_file)) {
+            $args[] = '-c';
+            $args[] = $xml_file;
+        }
+
+        $args[] = $tests_path;
+        $_SERVER['argv'] = $args;
 
         \PHPUnit\TextUI\Command::main();
     }
